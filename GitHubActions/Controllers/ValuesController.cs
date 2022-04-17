e0dd6f6c7e3ff2434c00dd56e67aca79b7aa710f
Ratomir Vukadin <ratomirvu@maestralsolutions.com>
2022-04-17 16:44:16 +0200

    config values

---
+++ GitHubActions/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubActions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult GetVersion()
+        {
+            return Ok(_configuration.GetValue<string>("Version"));
+        }
+
+        [Route("connection_string")]
+        public IActionResult GetConnectionString()
+        {
+            return Ok(_configuration.GetConnectionString("MySQL"));
+        }
+    }
+}
